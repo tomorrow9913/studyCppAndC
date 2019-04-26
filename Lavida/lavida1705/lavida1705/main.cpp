e005d354f4cd061296a7bd96494f8e32a1693b8b
JEONG MinGyu <tomorrow9913@gmail.com>
2019-04-26 16:07:35 +0900

    solved 4 problem in lavida.

---
+++ Lavida/lavida1705/lavida1705/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int input1, input2, result;
+
+	scanf("%d %d", &input1, &input2);
+
+	result = input1 * input2;
+
+	printf("%d\n", result);
+
+	return 0;
+}
---
+++ Lavida/lavida1713/lavida1713/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	int ainu7, megalusion, result;
+
+	scanf("ainu7: %d, megalusion: %d", &ainu7, &megalusion);
+
+	result = ainu7 + megalusion;
+
+	printf("%d", result);
+}

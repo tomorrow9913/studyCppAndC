77f36a3e4f89dfc22a39799883d1ea606148c6c5
tomorrow99 <tomorrow9913@gmail.com>
2019-05-31 20:48:06 +0900

    lifeGame

--- etc/lifeGame/main.cpp
+++ etc/lifeGame/main.cpp
@@ -1,16 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
 
 int main() {
     int testCase, count = 1;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),11);
+    printf("    /￣/         /￣/    /￣￣￣￣/   /￣￣￣/");
+    printf("   /  /          ￣     /   /￣￣￣  /  /￣￣");
+    printf("  /  /         /￣/    /    ￣￣/   /   ￣￣/");
+    printf(" /  /_____    /  /    /   /￣￣    /  /￣￣");
+    printf("/_________/  /__/    /__ /        /   ￣￣/");
+    printf("                                  ￣￣￣￣");
+    printf("              /￣￣￣/     /￣￣\\     |￣\\  /￣|      /￣￣￣/");
+    printf("             /  /￣￣     /  /\\  \\    | \\ \\/ / |     /  /￣￣");
+    printf("             | \\    __   /   ￣   \\   | |\\__/| |    /   ￣￣/");
+    printf("             \\  \\___||  /  /￣￣\\  \\  | |    | |   /  /￣￣");
+    printf("              \\______/ /__/      \\__\\ |_|    |_|  /   ￣￣/");
+    printf("                                                  ￣￣￣￣");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),7);
+    _sleep(100);
+    system("cls");
+
+    printf("Enter Generation (-1 is infinite) : ");
     scanf("%d", &testCase);
+    system("cls");
+
+    int preGen[30][30] = { 0, };
+    int nextGen[30][30] = { 0, };
+
+    printf("Please Enter a map using 0 and 1. (20x20) : \n");
+
+    for (int i = 0; i < 400; ++i) {
+        scanf("%d", &preGen[5 + i / 20][5 + i % 20]);
+    }
 
     while (testCase--){
-        int preGen[30][30] = { 0, };
-        int nextGen[30][30] = { 0, };
-
-        for (int i = 0; i < 400; ++i) {
-            scanf("%d", &preGen[5 + i / 20][5 + i % 20]);
-        }
         for (int i = 0; i < 400; ++i) {
             int neighbor = 0;
 
@@ -32,13 +56,25 @@ int main() {
                 nextGen[5 + i / 20][5 + i % 20] = 0;
         }
 
-        printf("<#%d 2nd Generation>\n", count++);
+        preGen[30][30] = { 0, };
+        system("cls");
+        printf("<#%dth Generation>\n", count++);
+        printf("+----------------------------------------+\n");
         for (int i = 0; i < 20; ++i) {
+            printf("|");
             for (int j = 0; j < 20; ++j) {
-                printf("%d ", nextGen[i + 5][j + 5]);
+                if(nextGen[i + 5][j + 5]) {
+                    printf("O ");
+                } else {
+                    printf("  ");
+                }
+                preGen[i + 5][j + 5] = nextGen[i + 5][j + 5];
             }
+            printf("|");
             printf("\n");
         }
+        printf("+----------------------------------------+\n");
+        _sleep(100);
     }
     return 0;
 }

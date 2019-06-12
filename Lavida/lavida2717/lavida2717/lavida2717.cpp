6087886f053069f7d6193159de6d93ce4acc472d
tomorrow9913 <tomorrow9913@gmial.com>
2019-06-12 22:30:35 +0900

    lavida

---
+++ Lavida/lavida2717/lavida2717/lavida2717.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <string.h>
+#pragma warning (disable : 4996)
+
+int main() {
+	int n;
+	double sum = 0;
+	scanf("%d", &n);
+
+	for (int i = 0; i < 5; i++)
+	{
+		double input, max = 0;
+		for (int j = 0; j < n; j++)
+		{
+			scanf("%lf", &input);
+			if (input > max)
+			{
+				max = input;
+			}
+		}
+		sum += max;
+
+	}
+
+	printf("%.3lf\n", sum);
+	return 0;
+}
